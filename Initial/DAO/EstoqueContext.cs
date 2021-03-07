c363c48c6a556675dd3752733a805e5fac7e4235
Matheus <matheus.op20@aluno.ifsc.edu.br>
2021-03-07 20:39:42 -0300

    categorias and Product Controller

---
+++ Initial/DAO/EstoqueContext.cs
@@ -0,0 +1,20 @@
+﻿using Initial.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Initial.DAO
+{
+    public class EstoqueContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        public DbSet<CategoriaDoProduto> Categorias { get; set; }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        
+    }
+}
---
+++ Initial/Models/CategoriaDoProduto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Initial.Models
+{
+    public class CategoriaDoProduto
+    {
+        public int Id { get; set; }
+
+        public String Nome { get; set; }
+
+        public String Descricao { get; set; }
+    }
+}
